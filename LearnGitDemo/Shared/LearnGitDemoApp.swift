a7ba42edc80240819e8803510002222fcf0c6bf6
Shawn <1765528153@qq.com>
2021-08-07 11:45:51 +0800

    First Commit

---
+++ LearnGitDemo/Shared/LearnGitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitDemoApp.swift
+//  Shared
+//
+//  Created by 胡君皓 on 2021/8/7.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
